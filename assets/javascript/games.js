d83809702218a09e1e63c8df9b0d9b9b68c6efa3
Sarfulah Stewart <fu.stew@yahoo.com>
2019-07-03 15:33:45 -0400

    cleaned up the code, functions beginning to work.

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -2,45 +2,71 @@ var userText = document.getElementById("userchoice-text");
 console.log(userText);
 var lossesText = document.getElementById("losses-text");
 
-var words = ["matilda", "aladdin", "frozen", "big", "shrek", "annie", "jumanji", "hook", "pinocchio", "cinderella", "bambi"];
+var computerChoices = ["matilda", "aladdin", "frozen", "big", "shrek", "annie", "jumanji", "hook", "pinocchio", "cinderella", "bambi"];
 
 var wins = 0;
 var losses = 0;
 var wrong = [];
-var guessesLeft = [];
+var guessesLeft = 0;
 var randWord;
 var directionsText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var userChoiceText = document.getElementById("userchoice-text");
 var computerChoiceText = document.getElementById("computerchoice-text")
-var word = words[Math.floor(Math.random() * words.length)];
-console.log(word);
+var computerGuess;
 var underscoreText = document.getElementById("underscore-text")
+var underScore = [];
 
+// The resetGame() function
+function resetGame() {
+    guessesLeft = 10;
+    wrong = [];
 
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerGuess);
 
-var remainingLetters = word.length;
-
-while (remainingLetters > 0) {
-    alert(answerArray.join(" "));
-
-    var guess = prompt("Guess a letter, or click cancel");
-    if (guess === null) {
-        break;
-    } else if (guess.length !== 1) {
-        alert("Please enter a letter");
-    } else {
-        for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess)
-                answerArray[j] = guess;
-            remainingLetters--;
-        }
+    for (var i = 0; i < computerGuess.length; i++) {
+        underScore.push("_");
     }
+
+    underscoreText.textContent = underScore.join(" ");
 }
-// alert(answerArray.join(" "));
-// alert("Great Job!" + word);
+
+resetGame(); // runs resetGame() function
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    console.log(userGuess);
+
+
+    // Check to see if userGuess is in the word
+    if(computerGuess.indexOf(userGuess) != -1) {
+        // The letter is in the word
+        // Replace our underscores with a correct letter
+        console.log("Found a match!");
+
+        for(var i= 0; i < computerGuess.length; i++) {
+            if(userGuess == computerGuess[i]) {
+                underScore[i] = userGuess
+                console.log(underScore)
+            }
+        }
+
+// Have we won? If so reset the game, and increase wins by 1
+    } else {
+        // The letter is not in the word
+        console.log("No match found");
+        // Userguess gets added to wrong (arrayname.push(userguess))
+        // guessesLeft decreases by 1
+        // Have we lost? If so reset the game, and incease losses by 1
+    }
+
+        directionsText.textContent = "";
+
+        userChoiceText.textContent = userGuess;
+        computerChoiceText.textContent = computerGuess;
+        winsText.textContent = wins;
+        lossesText.textContent = losses;
+        underscoreText.textContent = underScore.join(" ");
+    }
